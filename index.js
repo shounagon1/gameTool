023729c2234ab5c53e203268ad38debbfab8d9a1
shounagon1 <shounagonnoikikata@outlook.com>
2024-09-06 05:01:40 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+const  io  = require("socket.io-client");
+const {Manager} = require("socket.io-client");
+//const parser = require("socket.io-parser");
+const customParser = require('./socketIO.customParser');
+
